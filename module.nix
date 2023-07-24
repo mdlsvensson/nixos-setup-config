ff2decf73566bb835a38e425a1c94f88931339ee
mdlsvensson <wilmer.lindau@gmail.com>
2023-07-24 17:07:40 +0200

    check for host type, removed many options

--- module.nix
+++ module.nix
@@ -4,17 +4,12 @@ let
 in rec {
   options.setupConfig = {
     host = mkOption {
-      type = types.str;
+      type = types.addCheck types.str (str: str == "desktop" || str == "laptop" || str == "server");
       example = "desktop";
-      description = "The host configuration to apply.";
-    };
-    system = mkOption {
-      type = types.str;
-      example = "x86_64-linux";
-      description = "The system architecture.";
+      description = "The configuration to apply.";
     };
     user = {
-      name = mkOption {
+      username = mkOption {
         type = types.str;
         example = "myUsername";
         description = "Login username.";
@@ -24,18 +19,6 @@ in rec {
         example = /home/myUsername;
         description = "Path to the user home directory.";
       };
-    };
-    timeZone = mkOption {
-      type = types.str;
-      default = "Europe/Stockholm";
-      example = "America/New_York";
-      description = "The system timezone.";
-    };
-    defaultLocale = mkOption {
-      type = types.str;
-      default = "en_US.UTF-8";
-      example = "en_US.UTF-8";
-      description = "The default locale setting.";
     };
     git = {
       userName = mkOption {
@@ -47,12 +30,6 @@ in rec {
         type = types.str;
         example = "myGitEmail@gmail.com";
         description = "The git email setting.";
-      };
-      initDefaultBranch = mkOption {
-        type = types.str;
-        default = "main";
-        example = "main";
-        description = "The default branch for git init.";
       };
     };
   };
