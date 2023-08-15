27328a633c9299412c7a53232af6e19fdd5962a8
mdlsvensson <wilmer.lindau@gmail.com>
2023-08-16 01:50:37 +0200

    drivers/kernel modules

--- module.nix
+++ module.nix
@@ -1,12 +1,29 @@
 { config, ... }: 
-let
-  cfg = config.setupConfig;
+let cfg = config.setupConfig;
 in rec {
   options.setupConfig = {
     host = mkOption {
       type = types.addCheck types.str (str: str == "desktop" || str == "laptop" || str == "server");
       example = "desktop";
       description = "The configuration to apply.";
+    };
+    kernelModules = mkOption {
+      type = types.listOf types.str;
+      default = [ ];
+      example = [ "amdgpu" ];
+      description = "https://github.com/NixOS/nixpkgs/blob/master/nixos/modules/system/boot/kernel.nix";
+    };
+    initrd.kernelModules = mkOption {
+      type = types.listOf types.str;
+      default = [ ];
+      example = [ "amdgpu" ];
+      description = "https://github.com/NixOS/nixpkgs/blob/master/nixos/modules/system/boot/kernel.nix";
+    };
+    xserver.videoDrivers = mkOption {
+      type = types.listOf types.str;
+      default = [ ];
+      example = [ "nvidia" ];
+      description = "Tell Xorg what video drivers to use.";
     };
     user = {
       username = mkOption {
@@ -32,5 +49,11 @@ in rec {
         description = "The git email setting.";
       };
     };
+  };
+
+  config = {
+    boot.kernelModules = cfg.kernelModules;
+    boot.initrd.kernelModules = cfg.initrd.kernelModules;
+    services.xserver.videoDrivers = cfg.xserver.videoDrivers;
   };
 }
